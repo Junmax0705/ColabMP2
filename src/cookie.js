dc32ccf8bcfd12bf215025652a32017d20a3192a
Junmax0705 <119088182+Junmax0705@users.noreply.github.com>
2023-02-14 10:34:36 +0800

    commit
    commit

---
+++ src/cookie.js
@@ -0,0 +1,14 @@
+const cookieBox = document.querySelector(".wrapper"),
+acceptBtn = cookieBox.querySelector("button");
+
+acceptBtn.onclick = ()=>{
+  document.cookie = "Junmaxie; max-age="+60*60*24*30;
+  if(document.cookie){ 
+    cookieBox.classList.add("hide"); 
+  }else{ 
+    alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+  }
+}
+let checkCookie = document.cookie.indexOf("Junmaxie"); 
+
+checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
